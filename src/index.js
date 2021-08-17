39a28cc7283e4bb51c53e6d86dc18feca75663c9
iDestar <53664560+iDestar@users.noreply.github.com>
2021-08-17 21:29:03 +0300

    webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import './styles/main.scss'
+import './images/product-photo-1@1x.jpg'
+
+console.log('that');
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path =  require ('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require ('clean-webpack-plugin')
+let mode = 'development';
+let target = 'web';
+
+if(process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist';
+}
+
+module.exports = {
+    mode: mode,
+    target: target,
+
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+rules: [
+    {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+    },
+    {
+        test:/\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader','sass-loader']
+    },
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+       }
+      }
+     ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(), 
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+      })
+     ],
+
+    devtool: 'source-map',
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+    },
+}

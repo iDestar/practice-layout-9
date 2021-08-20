60915c606554279d4bc25821e9fa75526b01b88d
iDestar <53664560+iDestar@users.noreply.github.com>
2021-08-20 18:06:13 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -20,18 +20,7 @@ module.exports = {
 
     module: {
 rules: [
-    {
-        test: /\.(woff(2)?|ttf|eot|)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      },
+
     
     {
         test: /\.html$/i,

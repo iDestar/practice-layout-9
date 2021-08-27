79a6d929070d8f103cb61aa6526faa2793fc20b1
iDestar <53664560+iDestar@users.noreply.github.com>
2021-08-27 18:27:55 +0300

    EventListner

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './styles/main.scss'
-import './images/product-photo-1@1x.jpg'
+import './styles/main.scss';
+import './images/product-photo-1@1x.jpg';
 
+const headerMain = document.querySelector('.main');
+const iconMenu = document.querySelector('.icon-menu');
+
+const iconMenuHandler = function () {
+  headerMain.classList.toggle('main__active');
+  console.log(1);
+};
+
+window.onload = function () {
+  window.addEventListener('resize', function () {
+    if (window.innerWidth < 1134) {
+      iconMenu.addEventListener('click', iconMenuHandler);
+    } else {
+      iconMenu.removeEventListener('click', iconMenuHandler);
+    }
+  });
+};

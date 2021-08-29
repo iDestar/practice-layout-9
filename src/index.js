fce7db97cf8e6fbba6a2bdc04b0ab9489507d4ad
iDestar <53664560+iDestar@users.noreply.github.com>
2021-08-30 01:53:15 +0300

    benefits

--- src/index.js
+++ src/index.js
@@ -6,30 +6,35 @@ const iconMenu = document.querySelector('.icon-menu');
 const iconSearch = document.querySelector('.actions__search-icon');
 const formSearch = document.querySelector('.actions__search-form');
 
+iconSearch.addEventListener('click', function () {
+  console.log(123);
+});
+
 const iconMenuHandler = function () {
   headerMain.classList.toggle('main__active');
   iconMenu.classList.toggle('_active');
 };
 
-const iconSearchHeandler = function () {
+const iconSearchHandler = function () {
+  console.log(1);
   formSearch.classList.toggle('search__form-active');
 };
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   iconMenu.addEventListener('click', iconMenuHandler);
-  iconSearch.addEventListener('click', iconSearchHeandler);
+  iconSearch.addEventListener('click', iconSearchHandler);
 } else {
   iconMenu.removeEventListener('click', iconMenuHandler);
-  iconSearch.removeEventListener('click', iconSearchHeandler);
+  iconSearch.removeEventListener('click', iconSearchHandler);
 }
 
 window.onload = function () {
   window.addEventListener('resize', function () {
     if (window.innerWidth < 1134) {
       iconMenu.addEventListener('click', iconMenuHandler);
-      iconSearch.addEventListener('click', iconSearchHeandler);
+      iconSearch.addEventListener('click', iconSearchHandler);
     } else {
-      iconSearch.removeEventListener('click', iconSearchHeandler);
+      iconSearch.removeEventListener('click', iconSearchHandler);
       iconMenu.removeEventListener('click', iconMenuHandler);
       iconMenu.classList.remove('_active');
       headerMain.classList.remove('main__active');

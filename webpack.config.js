7b2273d9272f77820298358ef13df605b75133b8
iDestar <53664560+iDestar@users.noreply.github.com>
2021-09-13 16:43:33 +0300

    404 start

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,58 @@
-const path =  require ('path');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require ('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 let mode = 'development';
 let target = 'web';
 
-if(process.env.NODE_ENV === 'production') {
-    mode = 'production';
-    target = 'browserslist';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
 }
 
 module.exports = {
-    mode: mode,
-    target: target,
+  mode: mode,
+  target: target,
 
-    output: {
-        path: path.resolve(__dirname, 'dist')
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    module: {
-rules: [
-
-    
-    {
+  module: {
+    rules: [
+      {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
-    {
+      {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
-    },
-    {
-        test:/\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader','sass-loader']
-    },
-    {
+      },
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-            loader: "babel-loader",
-       }
+          loader: 'babel-loader',
+        },
       },
-      
-     ]
-    },
+    ],
+  },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(), 
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-      })
-     ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/404.html',
+    }),
+  ],
 
-    devtool: 'source-map',
-    devServer: {
-        contentBase: "./dist",
-        hot: true,
-    },
-}
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
